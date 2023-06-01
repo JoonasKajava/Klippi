3eec3f26006dcca5e03ad0d768d364be2df7bf46
Joonas Kajava <me@joonaskajava.com>
2023-06-01 10:38:58 +0300

    progress sveltekit working quite nicely

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import { sveltekit } from '@sveltejs/kit/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [
-    svelte({configFile:'svelte.config.js'}),
+    sveltekit({configFile:'svelte.config.js'}),
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
