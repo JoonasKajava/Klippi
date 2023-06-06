ab9c6a7099c22ae59bd73eb4eada92040f0efac1
Joonas Kajava <me@joonaskajava.com>
2023-06-07 02:08:26 +0300

    Custom updater dialog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,23 @@
 <script type="ts">
     import { fly } from "svelte/transition";
     import TitleBar from "./TitleBar.svelte";
+    import Update from "$lib/components/Update.svelte";
+    import { checkUpdate } from "@tauri-apps/api/updater";
 
     export let data;
+
+    let should_update = false;
+    let update_manifest;
+
+    (async () => {
+        const { shouldUpdate, manifest } = await checkUpdate();
+        //should_update = shouldUpdate;
+        update_manifest = manifest;
+    })();
+
+    setTimeout(() => {
+        should_update = true;
+    }, 4000);
 </script>
 
 <TitleBar />
@@ -15,3 +30,7 @@
         <slot />
     </div>
 {/key}
+
+{#if should_update}
+    <Update open={should_update} manifest={update_manifest} />
+{/if}
