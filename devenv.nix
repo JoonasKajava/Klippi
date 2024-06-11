63a7248bdfa52ac9b1a7d8b627c41355e5671d51
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-11 22:06:02 +0300

    refactor(env): devenv now works

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,15 @@
   inputs,
   ...
 }: {
+  packages = with pkgs; [libarchive openssl libsoup cairo gdk-pixbuf pango atkmm gtk3 webkitgtk];
+
   languages.rust.enable = true;
-  languages.javascript.enable = true;
+  languages.javascript = {
+    enable = true;
+    npm = {
+      enable = true;
+      install.enable = true;
+    };
+  };
   languages.typescript.enable = true;
 }
