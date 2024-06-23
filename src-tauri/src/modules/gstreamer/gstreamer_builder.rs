a7fbf3952fc7fa54995acbe71f7d8b53f359a0c0
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2024-06-23 19:29:59 +0300

    feat(gstreamer): initial builder for gstreamer

--- src-tauri/src/modules/gstreamer/gstreamer_builder.rs
+++ src-tauri/src/modules/gstreamer/gstreamer_builder.rs
@@ -1,1 +1,42 @@
-pub struct GstreamerBuilder {}
+use std::path::PathBuf;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GstreamerBuilderError {
+    #[error("Input file has not been specified")]
+    InputFileMissing,
+    #[error("Gstreamer failed to initialize with error: {0}")]
+    GstreamerInitializationFailed(anyhow::Error),
+}
+
+pub struct GstreamerBuilder {
+    pub input_file: Option<PathBuf>,
+}
+
+impl GstreamerBuilder {
+    pub fn set_input(&mut self, input: impl Into<PathBuf>) {
+        // TODO: Check that the file exists?
+        self.input_file = Some(input.into());
+    }
+
+    pub fn build(self) -> Result<(), GstreamerBuilderError> {
+        gstreamer::init()
+            .map_err(|e| GstreamerBuilderError::GstreamerInitializationFailed(e.into()))?;
+
+        let binding = &self
+            .input_file
+            .ok_or(GstreamerBuilderError::InputFileMissing)?;
+
+        let uri = binding
+            .to_str()
+            .ok_or(GstreamerBuilderError::InputFileMissing)?;
+
+        let pipeline = gstreamer::parse::launch(&format!(
+            "uridecodebin uri={uri}
+"
+        ));
+
+        Ok(())
+    }
+}
